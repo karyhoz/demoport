cee3f5926030bc15e9e958f7f39bac69bf83ca6b
Adebyiy Bayi <you@example.com>
2024-02-11 12:59:14 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// Smooth Scrolling
+document.querySelectorAll('header nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Toggle Navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
