b071fed78d9a54cc320b3149cab6f126a8cd874c
Adeniyi Bayo <Adeniyi Bayo>
2024-03-24 13:52:45 +0100

    change ininds

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
-// Smooth Scrolling
-document.querySelectorAll('header nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-// Toggle Navbar
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header navbar a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = wondow.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+navLinks.forEach(links => {
+    links.classList.remove('active');
+    document.querySelector('header nav a[href*=' + id + ']').classList.add('active'); 
+}); 
+        };
+    });
+
+// ==========sticky bar===========
+
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
 };
